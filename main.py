a3a873dc02500fc68d0a14a7884b5b4639bbc15a
Pedro Ferreira <plferreira4@gmail.com>
2013-05-27 19:16:39 +0100

    - add a text element to show the messages from the livestreamer program - improve the widget's layout

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
 import sys
-import subprocess
 
-from PySide.QtGui import QApplication, QLabel, QPushButton, QLineEdit, QHBoxLayout, QWidget
+from PySide.QtGui import QApplication, QLabel, QPushButton, QLineEdit, QHBoxLayout, QWidget, QTextEdit, QVBoxLayout
+from PySide.QtCore import QProcess
+
 
 '''
     to doo:
 
-        - add a text element that shows the messages from the livestreamer program
         - be able to save the links (to be easy later to open the stream again, without having to write it again)
         - maybe only have a single entry (QLineEdit) for the url and quality?..
 '''
 
 
 
+
 URL = None
 QUALITY = None
 PROCESS = None
+MESSAGES = None
 
 
 def go():
 
     global URL
     global QUALITY
+    global MESSAGES
+
 
     app = QApplication( sys.argv )
 
     URL = QLineEdit()
     QUALITY = QLineEdit()
+    MESSAGES = QTextEdit()
+
+
 
     URL.returnPressed.connect( start_stream )
     QUALITY.returnPressed.connect( start_stream )
 
-    layout = QHBoxLayout()
+    mainLayout = QVBoxLayout()
+    urlLayout = QHBoxLayout()
 
-    layout.addWidget( URL )
-    layout.addWidget( QUALITY )
+    urlLayout.addWidget( URL )
+    urlLayout.addWidget( QUALITY )
+
+    mainLayout.addLayout( urlLayout )
+    mainLayout.addWidget( MESSAGES )
 
 
     window = QWidget()
 
-    window.setLayout( layout )
+    window.setLayout( mainLayout )
     window.setWindowTitle( 'Live Streamer' )
     window.show()
 
@@ -54,13 +65,26 @@ def start_stream():
     url = URL.text()
     quality = QUALITY.text()
 
-    print( 'starting: {} {}'.format( url, quality ) )
+    # print( 'starting: {} {}'.format( url, quality ) )
 
     program = 'livestreamer'
 
-    subprocess.call([ program, url, quality ])
+    PROCESS = QProcess()
+    PROCESS.setProcessChannelMode( QProcess.MergedChannels )
+
+    PROCESS.start( program, [ url, quality ] )
+
+    PROCESS.readyReadStandardOutput.connect( showMessages )
 
 
+
+def showMessages():
+
+    outputBytes = PROCESS.readAll().data()
+
+    outputUnicode = outputBytes.decode( 'utf-8' )
+
+    MESSAGES.append( outputUnicode )
 
 
 if __name__ == '__main__':
