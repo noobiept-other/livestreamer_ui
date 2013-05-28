b5bcb8916f8952e41eedf20f0ef4cb9852a45da8
Pedro Ferreira <plferreira4@gmail.com>
2013-05-28 23:49:11 +0100

    - run the livestreamer program with the --json option first, to determine if the url/quality string given is valid, and if the quality isn't given, what are the available options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 from PySide.QtGui import QApplication, QLabel, QPushButton, QLineEdit, QHBoxLayout, QWidget, QTextEdit, QVBoxLayout, QListWidget, QListWidgetItem
 from PySide.QtCore import QProcess
@@ -9,7 +10,9 @@ from PySide.QtCore import QProcess
 
         - be able to save the links (to be easy later to open the stream again, without having to write it again)
         - maybe only have a single entry (QLineEdit) for the url and quality?..
-        - tell which qualities are available
+        - tell which qualities are available (and show them in a widget to be able to change easily between them)
+        - be able to call the livestreamer with its other arguments (like '--version')
+        - be able to clear the messages
 '''
 
 
@@ -32,8 +35,8 @@ class LiveStreamer:
 
             # set the events
 
-        url.returnPressed.connect( self.start_stream )
-        quality.returnPressed.connect( self.start_stream )
+        url.returnPressed.connect( self.parse_url )
+        quality.returnPressed.connect( self.parse_url )
         links.itemClicked.connect( self.select_stream )
 
         mainLayout = QVBoxLayout()
@@ -52,42 +55,116 @@ class LiveStreamer:
         window.setWindowTitle( 'Live Streamer' )
         window.show()
 
-        self.url = url
-        self.quality = quality
-        self.messages = messages
-        self.links = links
-        self.window = window
+        self.url_ui = url
+        self.quality_ui = quality
+        self.messages_ui = messages
+        self.links_ui = links
+        self.window_ui = window
+
+        self.stream_url = ''
+        self.stream_quality = ''
+
+
+    def parse_url( self ):
+
+        """
+            Gets the values from the ui elements, and executes the program in json mode, to determine if the values are valid
+        """
+
+        url = self.url_ui.text()
+        quality = self.quality_ui.text()
+
+        self.messages_ui.append( 'Trying to open stream: {} {}'.format(url, quality) )
+
+        arguments = []
+        process_function = self.determine_if_valid_url  # function to call with the live-streamer's output
+
+
+        if url:
+            arguments.append( '--json' )
+            arguments.append( url )
+
+
+                # if quality isn't provided, the program gives the possible quality values
+            if quality:
+                arguments.append( quality )
+
+            # if url is not given, show the help
+        else:
+            arguments.append( '--help' )
+            process_function = self.show_messages   # nothing to check, so just show the messages the program gives
+
+
+        self.stream_url = url
+        self.stream_quality = quality
+
+            # we'll call live-streamer two times, one to get the json info, and the other to actually start the stream
+        process = QProcess()
+
+        if url:
+            self.process_json = process
+
+        else:
+            self.process = process
+
+        process.setProcessChannelMode( QProcess.MergedChannels )
+        process.start( 'livestreamer', arguments )
+        process.readyReadStandardOutput.connect( process_function )
+
+
+    def determine_if_valid_url( self ):
+
+        """
+            Determines if the url and quality are valid values
+        """
+
+        outputBytes = self.process_json.readAll().data()
+
+        outputUnicode = outputBytes.decode( 'utf-8' )
+
+        try:
+            outputObject = json.loads( outputUnicode )
+
+        except ValueError as errorMessage:
+            print( errorMessage )
+            self.messages_ui.append( outputUnicode )
+            return
+
+
+        if outputObject.get( 'error' ):
+
+            self.messages_ui.append( outputObject[ 'error' ] )
+
+            # the quality wasn't provided
+        elif outputObject.get( 'streams' ):
+            qualityAvailable = ''
+
+            for quality in outputObject['streams']:
+                qualityAvailable += quality + ' '
+
+
+            self.messages_ui.append( 'Need to specify the quality of the stream.\nQualities Available: {}'.format( qualityAvailable ) )
+
+        else:
+            self.messages_ui.append( 'Opening the stream.' )
+            self.start_stream()
+
 
 
     def start_stream( self ):
 
-        url = self.url.text()
-        quality = self.quality.text()
-
-        arguments = []
-
-            # don't add if the argument isn't provided (if url is not given, the program gives a 'help' text)
-        if url:
-            arguments.append( url )
-
-            # if quality isn't provided, the program gives the possible quality values
-        if quality:
-
-            arguments.append( quality )
-
-        else:
-            self.messages.append( '< Need to specify the quality of the stream. >' )
-
-
-        program = 'livestreamer'
+        """
+            Assumes self.stream_url and self.stream_quality have valid values
+        """
 
         process = QProcess()
 
         self.process = process
 
         process.setProcessChannelMode( QProcess.MergedChannels )
-        process.start( program, arguments )
+        process.start( 'livestreamer', [ self.stream_url, self.stream_quality ] )
         process.readyReadStandardOutput.connect( self.show_messages )
+
 
 
     def show_messages( self ):
@@ -96,7 +173,7 @@ class LiveStreamer:
 
         outputUnicode = outputBytes.decode( 'utf-8' )
 
-        self.messages.append( outputUnicode )
+        self.messages_ui.append( outputUnicode )
 
 
     def select_stream( self, listWidgetItem ):
